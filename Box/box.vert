fd87330f3e7b37a592b8d897b01390f09ffd587a
Reynaldev <reynaldy.nur.cahyo.003@gmail.com>
2024-02-27 19:57:55 +0700

    Finished

--- Box/box.vert
+++ Box/box.vert
@@ -3,7 +3,6 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
-uniform mat4 uTransform;
 uniform mat4 uModel;
 uniform mat4 uView;
 uniform mat4 uProj;
@@ -13,6 +12,6 @@ out vec2 texCoord;
 
 void main()
 {
-	gl_Position = vec4(aPos, 1.0);
+	gl_Position = uProj * uView * uModel * vec4(aPos, 1.0);
 	texCoord = aTexCoord;
 }
--- Box/main.cpp
+++ Box/main.cpp
@@ -196,6 +196,8 @@ public:
 
 	void createTexture(const char *file, GLuint s, GLuint t, GLuint min, GLuint mag, GLuint fmt = GL_RGB)
 	{
+		glGenTextures(1, &texture);
+
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture);
 
@@ -266,6 +268,8 @@ int main()
 		return -1;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
 
@@ -276,12 +280,31 @@ int main()
 	box.createBuffer();
 	box.createTexture("container.jpg", GL_REPEAT, GL_REPEAT, GL_LINEAR, GL_LINEAR);
 
+	glm::mat4 model(1.0f);
+	glm::mat4 view(1.0f);
+	glm::mat4 projection(1.0f);
+
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+	unsigned int modelLoc = glGetUniformLocation(box.getProgram(), "uModel");
+	unsigned int viewLoc = glGetUniformLocation(box.getProgram(), "uView");
+	unsigned int projLoc = glGetUniformLocation(box.getProgram(), "uProj");
+
 	while (!glfwWindowShouldClose(window))
 	{
+		double t = glfwGetTime();
+
 		glClearColor(0.2f, 0.3f, 0.4f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		box.draw();
+
+		model = glm::rotate(model, (float)(t / 1000) * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
